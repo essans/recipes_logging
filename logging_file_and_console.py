6bc489118400d18c327f2f73979726753e6c4710
essans <30513008+essans@users.noreply.github.com>
2025-05-17 12:55:39 -0400

    Update logging_file_and_console.py

--- logging_file_and_console.py
+++ logging_file_and_console.py
@@ -1,8 +1,6 @@
 import logging
 import datetime as dt
 
-import logging
-
 def set_logger(filename, file_mode='w', log_detail='INFO', conole=False):
     """
     Basic logging function:
@@ -15,7 +13,6 @@ def set_logger(filename, file_mode='w', log_detail='INFO', conole=False):
     for handler in logger.handlers[:]:
         logger.removeHandler(handler)
 
-    # Configure file logging
     logging.basicConfig(
         filename=filename,
         level=level,
@@ -24,7 +21,6 @@ def set_logger(filename, file_mode='w', log_detail='INFO', conole=False):
         datefmt='%Y-%m-%d %H:%M:%S'
     )
 
-    # Add console (stream) handler
     if console:
         console_handler = logging.StreamHandler()
         console_handler.setLevel(level)
@@ -34,4 +30,32 @@ def set_logger(filename, file_mode='w', log_detail='INFO', conole=False):
 
     return logger
 
+
+def set_ts_logger(filename, log_detail='INFO'): 
+    """
+    Basic logging function which outputs to a timestamped file:
+      filename: filename string to save in current dir or pass a full '/path/to/filename.log'
+      log_detail: log_detail: 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'
+    """
+    
+    level = getattr(logging, log_detail.upper(), logging.INFO) #default/failsafe is INFO
+  
+    logger = logging.getLogger()
+    for handler in logger.handlers[:]:
+        logger.removeHandler(handler)
+
+    logging.basicConfig(
+                    filename= f'{filename}_{dt.datetime.now().strftime('%Y%m%d_%H%M%S')}',
+                    level=level,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
+    if console:
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(level)
+        console_formatter = logging.Formatter('%(levelname)s - %(message)s')
+        console_handler.setFormatter(console_formatter)
+        logger.addHandler(console_handler)
+
+    return logger
 
