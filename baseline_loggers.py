5129c182d0512b3fe67ce48bded0bc0b6139519b
essans <30513008+essans@users.noreply.github.com>
2025-05-17 11:08:24 -0400

    Update baseline_loggers.py

--- baseline_loggers.py
+++ baseline_loggers.py
@@ -7,7 +7,7 @@ def set_logger(filename, file_mode='w', log_detail='INFO'):
     Basic logging function accepting user inputs:
       filename: filename string to save in current dir or pass a full '/path/to/filename.log'
       file_mode : 'a' for append, 'w' for write
-      log_detail: 'INFO' or 'DEBUG'
+      log_detail: 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'
     """
     
     level = getattr(logging, log_detail.upper(), logging.INFO) #default/failsafe is INFO
@@ -31,7 +31,7 @@ def set_ts_logger(filename, log_detail='INFO'):
     """
     Basic logging function which outputs to a timestamped file:
       filename: filename string to save in current dir or pass a full '/path/to/filename.log'
-      log_detail: 'INFO' or 'DEBUG'
+      log_detail: log_detail: 'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'
     """
 
     
